0f53f1f40a64235ea97e8b8e7563f977f899c725
Victor Micheni <victormichenim@gmail.com>
2023-05-24 12:40:21 +0300

    Added section 2 and js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+section1 = document.getElementById("section1");
+section2 = document.getElementById("section2");
+
+function load() {
+    section1.style.display = "block";
+    section2.style.display = "none";
+}
+
+function displaySec1() {
+    section1.style.display = "block";
+    section2.style.display = "none";
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, FIrefox , Opera etc..
+}
+
+function displaySec2() {
+    section1.style.display = "none";
+    section2.style.display = "block";
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, FIrefox , Opera etc..
+}
+
+window.onload = load();
